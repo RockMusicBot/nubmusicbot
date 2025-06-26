26d5fa057309ac852b37fea44e693255828de442
RockMusicBot <githubmusicbot@gmail.com>
2025-06-26 08:37:40 +0530

    tools.py

--- tools.py
+++ tools.py
@@ -1134,31 +1134,52 @@ AudioQuality.STUDIO,
                 InlineKeyboardButton(text="▢", callback_data="end"),
             ],
         [                                                                                           InlineKeyboardButton(
-                text=f"{smallcap('Add to group')}", url=f"https://t.me/{clients["bot"].me.username}?startgroup=true"
-            ),InlineKeyboardButton(
-                text="✖ Close",callback_data="close"
-            )
-        ],
-        ])
-        sent_message = await clients["bot"].send_photo(
-            message.chat.id, thumb, play_styles[int(gvarstatus(OWNER_ID, "format") or 11)].format(lightyagami(mode),
-f"[{lightyagami(title)}](https://t.me/{clients["bot"].me.username}?start=vidid_{extract_video_id(youtube_link)})" if not os.path.exists(youtube_link) else lightyagami(title), duration, by.mention()),
-            reply_markup=keyboard        )
-        asyncio.create_task(autoleave_vc(sent_message, duration,chat))
-        asyncio.create_task(update_progress_button(sent_message, duration,chat))
-        try:
-            await message.delete()
-        except Exception as e:
-            logger.info(e)
-    except NoActiveGroupCall:
-        await clients["bot"].send_message(chat.id, "ERROR: No active group calls")
-        return await remove_active_chat(message.chat.id)
-    except GroupcallForbidden:
-        await clients["bot"].send_message(chat.id, "ERROR: Telegram internal server error")
-        return await remove_active_chat( message.chat.id)
-    except Exception as e:
-        await clients["bot"].send_message(chat.id, f"ERROR: {e}")
-        return await remove_active_chat(message.chat.id)
+                InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton(
+            text=f"{smallcap('Add to group')}", 
+            url=f"https://t.me/{clients['bot'].me.username}?startgroup=true"
+        ),
+        InlineKeyboardButton(
+            text="✖ Close", 
+            callback_data="close"
+        )
+    ],
+])
+
+sent_message = await clients['bot'].send_photo(
+    message.chat.id,
+    thumb,
+    play_styles[int(gvarstatus(OWNER_ID, "format") or 11)].format(
+        lightyagami(mode),
+        f"[{lightyagami(title)}](https://t.me/{clients['bot'].me.username}?start=vidid_{extract_video_id(youtube_link)})"
+        if not os.path.exists(youtube_link)
+        else lightyagami(title),
+        duration,
+        by.mention()
+    ),
+    reply_markup=keyboard
+)
+
+asyncio.create_task(autoleave_vc(sent_message, duration, chat))
+asyncio.create_task(update_progress_button(sent_message, duration, chat))
+
+try:
+    await message.delete()
+except Exception as e:
+    logger.info(e)
+
+except NoActiveGroupCall:
+    await clients['bot'].send_message(chat.id, "ERROR: No active group calls")
+    return await remove_active_chat(message.chat.id)
+
+except GroupcallForbidden:
+    await clients['bot'].send_message(chat.id, "ERROR: Telegram internal server error")
+    return await remove_active_chat(message.chat.id)
+
+except Exception as e:
+    await clients['bot'].send_message(chat.id, f"ERROR: {e}")
+    return await remove_active_chat(message.chat.id)
 
 
 
